bceaff693e49306cac3c0f13a7395acf46438f72
Jacopo Rodighiero <jacoporodighiero@Jacopos-MacBook-Pro-2.local>
2021-06-10 16:41:51 +0200

    added dependencies and hbs with email template

---
+++ src/mail/mail.module.ts
@@ -0,0 +1,33 @@
+import { MailerModule } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import { Module } from '@nestjs/common';
+import { MailService } from './mail.service';
+import { join } from 'path';
+
+@Module({
+  imports: [
+    MailerModule.forRoot({
+      transport: {
+        host: 'adidas.jacopo.com',
+        secure: false,
+        auth: {
+          user: 'jacopo@adidas.com',
+          pass: 'topsecret',
+        },
+      },
+      defaults: {
+        from: '"No Reply" <noreply@jacopoadidas.com>',
+      },
+      template: {
+        dir: join(__dirname, 'templates'),
+        adapter: new HandlebarsAdapter(),
+        options: {
+          strict: true,
+        },
+      },
+    }),
+  ],
+  providers: [MailService],
+  exports: [MailService],
+})
+export class MailModule {}
